63125fc010aad799b15229a5ddfcb748c9a4f94b
Mafiaboy94 <150351595+Mafiaboy94@users.noreply.github.com>
2023-11-09 11:48:10 -0600

    Update app.js

--- app.js
+++ app.js
@@ -384,7 +384,7 @@ galaxysim.meshVfx = new THREE.PointCloud();
 
     function displayGUI() {
             var testParameters = function() {
-                this.backgrounds = galaxysim.milky_way;
+                this.backgrounds = galaxysim.light_blue;
                 this.gravity_strength = 6.5;
                 this.large_stars_opacity = 0.6;
                 this.small_stars_opacity = 0.6;
