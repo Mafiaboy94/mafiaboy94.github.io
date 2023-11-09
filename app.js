17dbdb40b2e5e6f936d06e8d1604362202100410
Mafiaboy94 <150351595+Mafiaboy94@users.noreply.github.com>
2023-11-09 11:55:04 -0600

    Add files via upload

--- app.js
+++ app.js
@@ -384,7 +384,7 @@ galaxysim.meshVfx = new THREE.PointCloud();
 
     function displayGUI() {
             var testParameters = function() {
-                this.backgrounds = galaxysim.light_blue;
+                this.backgrounds = Light Blue;
                 this.gravity_strength = 6.5;
                 this.large_stars_opacity = 0.6;
                 this.small_stars_opacity = 0.6;
